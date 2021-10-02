8508209150321f7a8f52c4a6593b30d855a3dd28
NITHI GD <g.d.nithi5@gmail.com>
2021-10-02 23:56:58 +0530

    Error Fixed

--- components/Projects.js
+++ components/Projects.js
@@ -6,7 +6,7 @@ export default function Projects({ projects }) {
         <div className="bg-white p-3 relative my-6 shadow-md ">
             <h4 className="text-2xl text-gray-700 font-medium cursor-pointer hover:text-gray-800 text-center mb-3">Our Projects </h4>
             {
-                projects.map((proj)=><Project {...proj} />)
+                projects.map((proj)=><Project {...proj} key={proj.title} />)
             }
         </div>
     )
