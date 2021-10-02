883653c3c87d2ddbe611720d8c166323c56873da
NITHI GD <g.d.nithi5@gmail.com>
2021-10-02 22:26:36 +0530

    size and position of landing page corrected

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -28,44 +28,43 @@ export default function LandingPage({ children }) {
     return (
         <>
             <Header />
-                {/* main */}
-                <div className="w-full overflow-hidden bg-center bg-auto bg-fixed bg-no-repeat"
-                >
-
-                    <div className="flex flex-col items-center relative">
-                        <div className="w-full h-full absolute">
-                            <div className="w-full h-96  relative">
-                                <Image src="/images/modern.jpeg" layout="fill" objectFit="cover" objectPosition="center" alt="interior design" />
-                            </div>
-                        </div>
-                        <div className="h-28" />
-                        <div className="absolute h-full w-full bg-white opacity-50 z-0" />
-                        <div className="text-2xl font-medium z-20 mb-3">PVC Interiors</div>
-                        <div className=" text-3xl text-center md:text-6xl font-black tracking-widest z-10 text-gray-900 justify-end">
-                            Best Interior Designers in India</div>
-                        <Link href="/">
-                            <a className="btn transform font-medium border-2 rounded-md my-4" >Book Now</a>
-                        </Link>
-                        <div className="text-2xl text-center font-semibold md:text-4xl text-brown-dark flex items-center gap-2 transition
-                         transform duration-300 hover:scale-110 cursor-pointer">
-                            <span>Call Now : 7810005053</span>
-                            <Phone className="stroke-0 stroke-current" />
-                        </div>
-                        <div className="p-3 text-center text-sm md:text-lg z-20 font-medium text-gray-800 mb-3">
-                            Check out our Locations and Book your a amazing interior designs for your lovely house.
+            {/* main */}
+            <div className="w-full overflow-hidden bg-center bg-auto bg-fixed bg-no-repeat mb-4"
+            >
+                <div className="flex flex-col items-center relative">
+                    <div className="absolute w-full h-full">
+                        <div className="relative" style={{ width: "100%", height: "100%" }}>
+                            <Image src="/images/modern.jpeg" layout="fill" objectFit="cover" objectPosition="center" alt="interior design" />
                         </div>
                     </div>
+                    <div className="h-28" />
+                    <div className="absolute h-full w-full bg-white opacity-50 z-0" />
+                    <div className="text-2xl font-medium z-20 mb-3">PVC Interiors</div>
+                    <div className=" text-3xl text-center md:text-6xl font-black tracking-widest z-10 text-gray-900 justify-end">
+                        Best Interior Designers in India</div>
+                    <Link href="/">
+                        <a className="btn transform font-medium border-2 rounded-md my-4" >Book Now</a>
+                    </Link>
+                    <div className="text-2xl text-center font-semibold md:text-4xl text-brown-dark flex items-center gap-2 transition
+                         transform duration-300 hover:scale-110 cursor-pointer">
+                        <span>Call Now : 7810005053</span>
+                        <Phone className="stroke-0 stroke-current" />
+                    </div>
+                    <div className="p-3 text-center text-sm md:text-lg z-20 font-medium text-gray-800 mb-3">
+                        Check out our Locations and Book your a amazing interior designs for your lovely house.
+                    </div>
                 </div>
-                {/* Slider */}
-                {/* <Slider images={sliderImages} /> */}
-                {/* Gallery */}
-                <Gallery {...galleryItems} />
-                {/* Our Projects */}
+            </div>
+            {/* Slider */}
+            {/* <Slider images={sliderImages} /> */}
+            {/* Gallery */}
+            <Gallery {...galleryItems} />
+            {/* Our Projects */}
 
-                {/* Location */}
+            {/* Location */}
 
 
-                {/* About us */}
+            {/* About us */}
             {children}
 
             <Footer />
